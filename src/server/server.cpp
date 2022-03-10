bfab7fb497e824c5d4a1d315843e0b5c7a6e28fb
Eder Monteiro <eder.matheus.monteiro@gmail.com>
2022-03-10 01:24:09 -0300

    first cut of user login
    Signed-off-by: Eder Monteiro <eder.matheus.monteiro@gmail.com>

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -5,3 +5,19 @@
 #include <time.h>
 
 Server::Server() {}
+
+bool Server::loginUser(std::string username)
+{
+  if (users_.find(username) != users_.end()) { // user already exists on the db
+    User& user = users_[username];
+    if (user.getSessions() >= MAX_SESSIONS) {
+      return false;
+    }
+    user.incrementSessions();
+  } else { // creating new user
+    users_[username] = User(username);
+    users_[username].incrementSessions();
+  }
+
+  return true;
+}
