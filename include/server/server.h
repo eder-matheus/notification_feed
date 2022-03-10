014baf60ad4b226b44260ce6266645ddf6f3404a
Eder Monteiro <eder.matheus.monteiro@gmail.com>
2022-03-10 01:23:52 -0300

    update notification management
    Signed-off-by: Eder Monteiro <eder.matheus.monteiro@gmail.com>

--- include/server/server.h
+++ include/server/server.h
@@ -2,14 +2,19 @@
 #include "notification.h"
 #include "user.h"
 #include <string>
+#include <map>
 #include <unordered_map>
 
 class Server {
 private:
   std::unorderd_map<std::string, User> users_;
-  std::unorderd_map<std::string, std::vector<Notification>> received_notifications_;
+  // this map stores all the notifications created by all users
+  std::unorderd_map<long int, Notification> notifications_;
+  // this map stores the notifications that the user (key) have to receive
+  // the vector of notification ids should be sorted in crescent way
+  std::map<std::string, std::vector<long int>> pending_notifications_;
 
 public:
   Server();
-
+  bool loginUser(std::string username);
 };
