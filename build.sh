4084683113a50811934a631a8d7f0fd6c4e27933
Eder Monteiro <eder.matheus.monteiro@gmail.com>
2022-03-19 15:12:36 -0300

    add clear and nuke options to build
    Signed-off-by: Eder Monteiro <eder.matheus.monteiro@gmail.com>

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-mkdir build
-cd build
-cmake ..
-make
-cd -
+if [ "$1" = "clear" ]; then
+	rm -rf build
+elif [ "$1" = "nuke" ]; then
+	rm -rf build
+	rm -rf client_env
+	rm -rf server_env
+else
+	mkdir build
+	cd build
+	cmake ..
+	make -j 4
+	cd -
+fi
